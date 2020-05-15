684751c8d3a53fdb58f080db8e5a06c20b03502a
wulei19991221 <wulei7217@gmail.com>
2020-05-15 21:06:37 +0800

    优化代码,修复只显示一个答案

--- 蚂蚁庄园答案.py
+++ 蚂蚁庄园答案.py
@@ -3,58 +3,66 @@
 # @Author:吴磊
 # @Time: 2020年04月23日11时
 # @File: 蚂蚁庄园答案.py
-from requests_html import HTMLSession
+from lxml import etree
 import re
 import time
-
+import requests
+from print_color import *
 
 def get_max_page():
     max_page = 0
     try:
-        r = session.get(url)
-        last = r.html.find('.page-last', first=True).attrs['href']
-        rule = re.compile("_(.*?).html")
-        max_page = re.findall(rule, last)[0]
+        r = requests.get(url).text
+        htm = etree.HTML(r)
+        last = htm.xpath('.//div[@class="page-container"]/a[last()]/@href')[0]
+        max_page = re.findall("_(.*?).html", last)[0]
     except (AttributeError, TypeError):
-        print('请重新检查网址')
+        print_c(fred, '请重新检查网址')
     return max_page
 
 
 def all_answers(counts):
     all_url = [f'https://www.youxi369.com/news/2254_{i}.html' for i in range(counts, 1, -1)]
     all_url.append(url)
-    content = ''
-    content_list = []
     for i in all_url:
-        ps = session.get(i).html.find('.strategy-main-content p')
-        for p in ps:
-            if p.text not in content_list:
-                content_list.append(p.text)
-        content_list.sort()
-        for j in content_list:
-            content += j + '\n'
-        content_list.clear()
+        content = ''
+        r = requests.get(i).text
+        htm = etree.HTML(r)
+        ps = htm.xpath('.//div[@class="strategy-main-content"]/p')
+        for p in ps[::-1]:
+            text = ''.join(p.xpath('.//text()'))
+            if '月' in text and '日' in text:
+                content += text + '\n'
         content += '*' * 120 + '\n'
-    with open(f'蚂蚁庄园每日答案{time.strftime("%y-%m-%d")}.txt', mode='w', encoding='utf-8') as f:
-        f.write(content)
+        with open(f'蚂蚁庄园每日答案{time.strftime("%y-%m-%d")}.txt', mode='a', encoding='utf-8') as f:
+            f.write(content)
 
 
 def get_latest_answer():
-    ps = session.get(url).html.find('.strategy-main-content p')
-    print(ps[3].text)
+    current_time = list(time.strftime('%m月%d日'))
+    if current_time[0] == '0':
+        del current_time[0]
+    if current_time[-3] == '0':
+        del current_time[-3]
+    current_time = ''.join(current_time)
+    r = requests.get(url).text
+    htm = etree.HTML(r)
+    ps = htm.xpath('.//div[@class="strategy-main-content"]/p[position()=4 or position()=5]')
+    for p in ps:
+        todat_ans = ''.join(p.xpath('.//text()'))
+        if current_time in todat_ans:
+            print_c(fblue, todat_ans)
 
 
 def save_all_answer():
     page_num = int(get_max_page())
     all_answers(page_num)
-    print('save all successful')
+    print_c('save all successful')
 
 
 if __name__ == '__main__':
     start = time.time()
-    session = HTMLSession()
     url = 'https://www.youxi369.com/news/2254.html'
-    # get_latest_answer()
-    save_all_answer()
-    session.close()
-    print(f'{time.time() - start:.2f}')
+    get_latest_answer()
+    # save_all_answer()
+    print_c(f'耗时: {time.time() - start:.2f}秒')
